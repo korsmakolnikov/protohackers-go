99f3d4de91c0ae1780ee882d8a0093ee06405906
korsmakolnikov <korsmakolnikov@gmail.com>
2023-08-06 13:19:44 +0200

    Add tcp server --no request parsing yet

--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
 
+const (
+	HOST = "localhost"
+	PORT = "8080"
+	TYPE = "tcp"
+)
+
+func main() {
+	listen, err := net.Listen(TYPE, HOST+":"+PORT)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	fmt.Printf("listening for %s connection on %s:%s\n", TYPE, HOST, PORT)
+
+	defer listen.Close()
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//respond
+
+	conn.Close()
 }
